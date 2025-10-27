1b8241707a1f891ca1cec5b92bfccd47ec1b88ef
Mona Lisa <affanwhat@gmail.com>
2025-10-27 18:13:02 +0700

    Initial commit

---
+++ cmd/web/main.go
@@ -0,0 +1,61 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"net/http"
+	"time"
+
+	"github.com/affanwhat/bookings/pkg/config"
+	"github.com/affanwhat/bookings/pkg/handlers"
+	"github.com/affanwhat/bookings/pkg/render"
+	"github.com/alexedwards/scs/v2"
+)
+
+const portNumber = ":8081"
+var app config.AppConfig
+var session *scs.SessionManager
+
+func main() {
+	
+
+	// change this to true when in production
+	app.InProduction = false
+
+	session = scs.New()
+	session.Lifetime = 24 * time.Hour
+	session.Cookie.Persist = true
+	session.Cookie.SameSite = http.SameSiteLaxMode
+	session.Cookie.Secure = app.InProduction
+
+	app.Session = session
+
+	tc, err := render.CreateTemplateCache()
+	if err != nil {
+		log.Fatal("cannot create template cache")
+	}
+
+	app.TemplateCache = tc
+	app.UseCache = false
+
+	render.NewTemplates(&app)
+
+	repo := handlers.NewRepo(&app)
+	handlers.NewHandlers(repo)
+
+	/*
+	http.HandleFunc("/", handlers.Repo.Home)
+	http.HandleFunc("/about", handlers.Repo.About)
+	*/
+
+	fmt.Println(fmt.Sprintf("Starting application of port %s \n", portNumber))
+	//_ = http.ListenAndServe(portNumber, nil) // If there's an error, ignore it.
+
+	srv := &http.Server {
+		Addr: portNumber,
+		Handler: routes(&app),
+	}
+
+	err = srv.ListenAndServe()
+	log.Fatal(err)
+}
